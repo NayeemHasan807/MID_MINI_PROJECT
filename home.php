77f6da19d3551977fba47fa5b1eb3bdfc4de1a74
Nayeem <moonnayeem87@gmail.com>
2020-08-13 22:53:27 +0600

    Logged in home and logout system

---
+++ home.php
@@ -0,0 +1,71 @@
+<?php
+	session_start();
+	if(!empty($_SESSION))
+	{
+		if(empty($_SESSION['status']))
+		{
+			header('location:logout.php');
+		}
+	}
+	else
+	{
+		if(empty($_COOKIE['status']))
+		{
+			header('location:logout.php');
+		}
+	}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Home</title>
+</head>
+<body>
+	<table align="center" border="1">
+		<tr>
+			<td colspan="5" align="center">
+				<?php
+					if(!empty($_SESSION))
+					{
+						if($_SESSION['usertype']=="User")
+						{
+							echo "<h1>Welcome ".$_SESSION['name']."!</h1><br/>";
+							echo "<a href='profile.php'>Profile</a><br/>";
+							echo "<a href='changepassword.php'>Change Password</a><br/>";
+							echo "<a href='logout.php'>Logout</a><br/><br/>";
+						}
+						else
+						{
+							echo "<h1>Welcome ".$_SESSION['name']."!</h1><br/>";
+							echo "<a href='profile.php'>Profile</a><br/>";
+							echo "<a href='changepassword.php'>Change Password</a><br/>";
+							echo "<a href='viewusers.php'>View Users</a><br/>";
+							echo "<a href='logout.php'>Logout</a><br/><br/>";
+						}
+						
+					}
+					else
+					{
+						if($_COOKIE['usertype']=="User")
+						{
+							echo "<h1>Welcome ".$_COOKIE['name']."!</h1><br/>";
+							echo "<a href='profile.php'>Profile</a><br/>";
+							echo "<a href='changepassword.php'>Change Password</a><br/>";
+							echo "<a href='logout.php'>Logout</a><br/><br/>";
+						}
+						else
+						{
+							echo "<h1>Welcome ".$_COOKIE['name']."!</h1><br/>";
+							echo "<a href='profile.php'>Profile</a><br/>";
+							echo "<a href='changepassword.php'>Change Password</a><br/>";
+							echo "<a href='viewusers.php'>View Users</a><br/>";
+							echo "<a href='logout.php'>Logout</a><br/><br/>";
+						}
+					}
+				?> 
+			</td>
+		</tr>
+		
+	</table>
+</body>
+</html>

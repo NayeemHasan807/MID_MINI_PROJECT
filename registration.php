2d95d9714847a3ee1dec35e0dfaa778624b4f8e1
Nayeem <moonnayeem87@gmail.com>
2020-08-13 22:22:32 +0600

    Regestration complete with database

--- registration.php
+++ registration.php
@@ -9,14 +9,26 @@
 		}
 		else 
 		{
-			if($_POST['password'] == $_POST['cpassword'])
+			$connection = mysqli_connect('127.0.0.1', 'root', '', 'miniproject');
+			$result= mysqli_query($connection, "select * from registration where id ='".$_POST['id']."';");
+			$data=mysqli_fetch_assoc($result);
+			mysqli_close($connection);
+			if(empty($data['id']))
 			{
-				
+				if($_POST['password'] == $_POST['cpassword'])
+				{
+					$connection = mysqli_connect('127.0.0.1', 'root', '', 'miniproject');
+					$result = mysqli_query($connection, "insert into `registration`(`id`, `password`, `name`, `email`, `usertype`) values ('".$_POST['id']."','".$_POST['password']."','".$_POST['name']."','".$_POST['email']."','".$_POST['usertype']."');");
+					mysqli_close($connection);
+					header('location:login.html');
+				}
+				else
+				{
+					echo "password & confirm password must need to be same!";
+				}
 			}
 			else
-			{
-				echo "password & Confirm password must need to be same!";
-			}
+				echo "This id is not available!";
 		}
 
 	}
